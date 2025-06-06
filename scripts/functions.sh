10684e25dfe7ec3c2a85d371543486936b327688
Chris Procter <cprocter@redhat.com>
2025-06-06 09:59:03 +0100

    build bundles for kmm c7931df

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -12,7 +12,13 @@ function version_zstream {
 }
 
 function latest_commit {
-    git rev-parse HEAD
+    local SHORT=""
+    if [ "$1" == "-s" ]; then
+        SHORT="--short"
+        shift
+    fi
+    local COMMIT=${1:-HEAD}
+    git rev-parse $SHORT $COMMIT
 
 }
 
@@ -22,8 +28,8 @@ function latest_kmm {
 
 
 function snapshots {
-    APP=$1
-    COMMIT=$2
+    local APP=$1
+    local COMMIT=$2
 
     if [ -n "$APP" ]; then
         oc get snapshot -o custom-columns=NAME:.metadata.name --no-headers -l pac.test.appstudio.openshift.io/sha=$COMMIT,appstudio.openshift.io/application=$APP --sort-by='{.metadata.creationTimestamp}' | tail -n 1
@@ -38,7 +44,7 @@ function snapshots {
 }
 
 function check_snapshot {
-    SNAPSHOT=$1
+    local SNAPSHOT=$1
 
     oc describe snapshot $SNAPSHOT | awk 'BEGIN{ while(("cat bundle-hack/*.yaml" | getline x) != 0){a[x]=1;}}/Container Image:/{if(a[$3]!=1){print $3} }'
 
@@ -46,14 +52,14 @@ function check_snapshot {
 
 
 function update_pullspecs {
-    VERSION=$(version_hyphen)
+    local VERSION=$(version_hyphen)
 
     oc get component --no-headers -o custom-columns=NAME:.metadata.name,IMAGE:.status.lastPromotedImage | awk -v x=$VERSION '$2!~/\<none\>/{gsub("\\-?v?"x,"",$1); print $2 > "bundle-hack/" $1 ".yaml"}'
 
 }
 
 function pipelineruns {
-    COMMIT=$1
+    local COMMIT=$1
     if [ -n "$COMMIT" ]; then
         oc get pipelineruns -l pipelinesascode.tekton.dev/sha=$COMMIT --sort-by='{.metadata.creationTimestamp}'
     else
@@ -67,8 +73,8 @@ function pipelineruns {
 #    oc get release --sort-by='{.metadata.creationTimestamp}' | awk -v r="${PATTERN}-${VERSION}-r" '$1~r{gsub(r, "",$1); if( (int($1)+1) > l){l=int($1)+1}}END{print r l}'
 #}
 function next_release {
-    KMM=${1/kmm-2-4/kmm-240}
-    APP=${2/kmm-2-4/kmm-240}
+    local KMM=${1/kmm-2-4/kmm-240}
+    local APP=${2/kmm-2-4/kmm-240}
     oc get release --sort-by='{.metadata.creationTimestamp}' | 
         awk -v kmm=$KMM -v app=$APP '
                 BEGIN{
@@ -99,7 +105,7 @@ function next_release {
 }
 
 function releaseplan {
-    APPLICATION=$1
+    local APPLICATION=$1
     oc get releaseplan | awk -v app=$APPLICATION '$2==app{print $1}'
 
 }
