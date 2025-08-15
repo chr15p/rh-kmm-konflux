96b5557cbfdfcd4ad4f9dcb657768d2b00c90276
Chris Procter <cprocter@redhat.com>
2025-08-15 11:31:19 +0100

    FBCs controlled from build_settings.conf

--- scripts/update_fbc.sh
+++ scripts/update_fbc.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-RELEASE=$(awk -F= '/RELEASE/{print $2}' build_settings.conf)
-PULLSPEC_FILE=${1:-operator-bundle.yaml}
-CATALOG_FILE=${2} # fbc/op/kernel-module-management/catalog.json
+PACKAGE=${1}
+RELEASE=${2}
+PULLSPEC_FILE=${3:-operator-bundle.yaml}
+CATALOG_FILE=${4:-fbc/op/${OPERATOR}/catalog.json}
 
 NEWIMAGE=$( cat ${PULLSPEC_FILE} )
-OLDIMAGE=$( sed -n "/kernel-module-management.v$RELEASE/,+3{/image/p}" $CATALOG_FILE | awk -F'\"' '{print $4}' )
+OLDIMAGE=$( sed -n "/${PACKAGE}.v${RELEASE}/,+3{/image/p}" $CATALOG_FILE | awk -F'\"' '{print $4}' )
 
 sed -i "s|$OLDIMAGE|$NEWIMAGE|" $CATALOG_FILE
